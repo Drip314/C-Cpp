2c29f422436013ab2b2e1671868ca0312cef0fa3
Drip <96818219+Drip314@users.noreply.github.com>
2025-02-28 11:01:09 +0100

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World";
+    return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+*\
